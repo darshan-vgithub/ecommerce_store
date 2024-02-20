b72832118342f785f94877161eb8f77191e0f02b
Darshan.V <darshanv487@gmail.com>
2024-02-20 12:38:45 +0530

    created a new user and got it stored in the database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,28 @@ import User from "../models/userModel.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(username);
-  console.log(email);
-  console.log(password);
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the inputs..");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("user already exists");
+
+  const newUser = new User({ username, email, password });
+  try {
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      password: newUser.password,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 export { createUser };
