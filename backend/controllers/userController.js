e6c21df5710d46ffde06decbc9e2dfe344c24023
Darshan.V <darshanv487@gmail.com>
2024-02-20 12:03:46 +0530

    created user routes and user controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
 
-const createUser = asyncHandler(async(req,res)=>{
-    res.send("hello")
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  console.log(username);
+  console.log(email);
+  console.log(password);
 });
 
-export {createUser}
+export { createUser };
